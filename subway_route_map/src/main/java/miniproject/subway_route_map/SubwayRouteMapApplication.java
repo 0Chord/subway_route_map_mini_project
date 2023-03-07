f8379630e991784f9398926676918951a86096ae
0Chord <seaweed.0chord@gmail.com>
2023-03-07 21:42:12 +0900

    feat: 추가 subway_route_map_mini_project 서버
    MSA를 위한 discovery_service, gateway_service, spring서버, django 서버 추가

---
+++ subway_route_map/src/main/java/miniproject/subway_route_map/SubwayRouteMapApplication.java
@@ -0,0 +1,13 @@
+package miniproject.subway_route_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayRouteMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubwayRouteMapApplication.class, args);
+	}
+
+}
---
+++ subway_route_map/src/test/java/miniproject/subway_route_map/SubwayRouteMapApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.subway_route_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayRouteMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
