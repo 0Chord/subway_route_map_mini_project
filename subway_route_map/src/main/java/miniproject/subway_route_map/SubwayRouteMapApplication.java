90cb7e54f7bdaa910756696126497245ca922059
0Chord <seaweed.0chord@gmail.com>
2023-03-19 21:05:33 +0900

    feat : 추가 msa gateway 연결

--- subway_route_map/src/main/java/miniproject/subway_route_map/SubwayRouteMapApplication.java
+++ subway_route_map/src/main/java/miniproject/subway_route_map/SubwayRouteMapApplication.java
@@ -2,8 +2,10 @@ package miniproject.subway_route_map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SubwayRouteMapApplication {
 
 	public static void main(String[] args) {
