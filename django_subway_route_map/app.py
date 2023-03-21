4dea1e6fb9ae6dc5e9a04de9b466002b78ae150b
0Chord <seaweed.0chord@gmail.com>
2023-03-21 22:18:50 +0900

    feat : 추가 flask 서버 eureka-client, gateway-service에 등록

--- django_subway_route_map/app.py
+++ django_subway_route_map/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import sys
-sys.path.append('/Users/0chord/Desktop/subway_route_map_mini_project/subway_route_map_mini_project/django_subway_route_map/route_map')
+import py_eureka_client.eureka_client as eureka_client
+
+sys.path.append(
+    "/Users/0chord/Desktop/subway_route_map_mini_project/subway_route_map_mini_project/django_subway_route_map/route_map")
 from route_map.dijkstra import dijkstra
 
+rest_port = 8050
+
+eureka_client.init(eureka_server="http://localhost:8761/eureka",
+                   app_name="route-map-service",
+                   instance_port=rest_port)
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/route_map', methods=['GET'])
 ## flask에서 get 방식 parameter 요청 방법
@@ -20,4 +29,4 @@ def route_map():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=rest_port, debug=True)
