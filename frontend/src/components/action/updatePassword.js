dc0894b72e0bf1550c61e87e1dbe56a523fcafc5
0Chord <seaweed.0chord@gmail.com>
2023-06-17 20:29:32 +0900

    remove : 삭제 notification

--- frontend/src/components/action/updatePassword.js
+++ frontend/src/components/action/updatePassword.js
@@ -4,6 +4,5 @@ export async function updatePassword(data,setEmailError, setPasswordError){
     await axios.post("/user/update-password",data)
         .then(res=>{
             console.log(res.data)
-            new Notification("비밀번호변경",{body:res.data})
         })
 }
